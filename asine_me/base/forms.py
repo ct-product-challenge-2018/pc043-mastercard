630a276041de5146eaca2be71ede1f16e22ff9b1
irenetung <yilingtung93@gmail.com>
2018-12-28 21:49:14 +0800

    Separate dev and prod settings files

--- asine_me/base/forms.py
+++ asine_me/base/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
--- asine_me/base/views.py
+++ asine_me/base/views.py
@@ -2,24 +2,28 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from .forms import ContactForm
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
 
 def home(request):
     return render(request, 'base/home.html')
 
+
 def welcome(request):
     return render(request, 'base/welcome.html')
+
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email,
-                          ['test@test.com'])
+                nameEmailMessage = "Full name: {} {}\nEmail: {}\n\n{}".format(first_name, last_name, from_email, message)
+                send_mail(subject, nameEmailMessage, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
                 messages.success(request, 'Thank you for your message. We will get back to you shortly.')
                 return redirect('asineme_home')
             except BadHeaderError:
